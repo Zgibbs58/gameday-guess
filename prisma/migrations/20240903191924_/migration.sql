a3b4fc3d8009d89a28ae95709f029e927d324891
Zach Gibbs <zacharywgibbs@gmail.com>
2024-09-03 14:20:20 -0500

    updated schema to handle data for total players and winner features.

--- prisma/migrations/20240903191924_/migration.sql
+++ prisma/migrations/20240903191924_/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "score" INTEGER NOT NULL,
+    "winner" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -10,7 +11,15 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "TeamScore" (
     "id" SERIAL NOT NULL,
-    "score" INTEGER NOT NULL,
+    "score" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "TeamScore_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TotalPlayers" (
+    "id" SERIAL NOT NULL,
+    "value" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "TotalPlayers_pkey" PRIMARY KEY ("id")
 );
